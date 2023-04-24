01c61df123644337cc90517dbbd591d26af9c76e
yingWWen <101466133+yingWWen@users.noreply.github.com>
2023-04-24 12:01:24 +0800

    Add files via upload

--- py_file/kdv_figure_other_solution_3_3.py
+++ py_file/kdv_figure_other_solution_3_3.py
@@ -1,7 +1,7 @@
+# Importing the necessary packages
 from matplotlib import pyplot as plt
 from scipy.integrate import solve_ivp
 import autograd.numpy.random as npr
-
 import autograd.numpy as np
 from Lie_NN.lieneuralsolver import NNSolver
 import seaborn as sns
@@ -9,6 +9,7 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 import time
 
+# Set picture parameters that can be modified
 plt.rcParams["figure.figsize"] = (6,4)
 plt.rcParams['legend.fontsize'] = 10
 
@@ -19,84 +20,46 @@ plt.rcParams['axes.titlesize'] = 15
 plt.rcParams['axes.labelsize'] = 10
 
 
-
-
-
 def f(t, y):
     # kdv equation other solution
     a = -4
     b = 6
-
-    X = y[0]
-    Y = y[1]
-    Z = y[2]
-
+    X = y[0]  #u1
+    Y = y[1]  #u2
+    Z = y[2]  #u3
     return [Y, Z, a * Y - b * X * Y]
 
-
+# Training Points t is \xi
 t = np.linspace(-3, 3, 250).reshape(-1, 1)
+
+# Initial value (boundary -3)
 y0_list = [-1.31360126, 0.03926899, 0.07776023]
 
+# Numerical solver
 sol = solve_ivp(f, [t.min(), t.max()], y0_list,
                 t_eval=t.ravel(), method='Radau', rtol=1e-5)
-
-bwith = 0.5
-ax = plt.gca()
-ax.spines['top'].set_color('black')
-ax.spines['right'].set_color('black')
-ax.spines['left'].set_color('black')
-ax.spines['bottom'].set_color('black')
-ax.spines['bottom'].set_linewidth(bwith)
-ax.spines['left'].set_linewidth(bwith)
-ax.spines['top'].set_linewidth(bwith)
-ax.spines['right'].set_linewidth(bwith)
-plt.scatter(sol.t, sol.y[0], s=2, marker='_', alpha=0.5,  label='$u_1$')
-plt.scatter(sol.t, sol.y[1], s=2,  marker='o', alpha=0.5, label='$u_2$')
-plt.scatter(sol.t, sol.y[2], s=2, marker = '+', alpha=0.5,  label = '$u_3$')
-plt.grid(which='major')
-plt.tick_params(top=False, bottom=True, left=True, right=False)
-plt.tick_params(which='both', direction='in', width=0.5, length=2)
-plt.axis([-3, 3, -1, 1])
-plt.xticks(np.arange(-3, 4, step=1))
-plt.yticks(np.arange(-2, 2, step=0.25))
 plt.xlabel('$t$', fontsize=10)
 plt.ylabel('$u$', fontsize=10)
-
+plt.plot(sol.t, sol.y[0],  label='$u_1$')
+plt.plot(sol.t, sol.y[1],   label='$u_2$')
+plt.plot(sol.t, sol.y[2],   label='$u_3$')
 plt.legend()
-plt.show()
+plt.show()   #Draw graph
 
-
-nn = NNSolver(f, t, y0_list, n_hidden=30)
+############################# Solver for the proposed method
+nn = NNSolver(f, t, y0_list, n_hidden=30)    #Number of neurons in the hidden layer 30
 print(nn)
 
 nn.reset_weights()
-nn.train(maxiter=2000, iprint=100)
+nn.train(maxiter=2000, iprint=100)     # Maximum number of iterations
 nn.loss_func(nn.params_list)
-print(nn.loss_func(nn.params_list))
-print(nn.params_list[0])
+print(nn.loss_func(nn.params_list))    # Print the optimal parameters
+print(nn.params_list[0])               # Print the optimal parameters of network 1
+nn.plot_loss()                         # Number of iterations and loss function image
 
-
+############################ The output of the constructed solution in the training set
 y_pred_list, dydt_pred_list = nn.predict()
 plt.figure(figsize= (8, 6))
-bwith = 0.5
-ax = plt.gca()
-ax.spines['top'].set_color('black')
-ax.spines['right'].set_color('black')
-ax.spines['left'].set_color('black')
-ax.spines['bottom'].set_color('black')
-ax.spines['bottom'].set_linewidth(bwith)
-ax.spines['left'].set_linewidth(bwith)
-ax.spines['top'].set_linewidth(bwith)
-ax.spines['right'].set_linewidth(bwith)
-
-plt.grid(which='major')
-plt.tick_params(top=False, bottom=True, left=True, right=False)
-plt.tick_params(which='both', direction='in', width=0.5, length=2)
-plt.axis([-3, 3, -1, 1])
-plt.xticks(np.arange(-3, 4, step=1))
-plt.yticks(np.arange(-2, 1, step=0.3))
-
-
 plt.plot(sol.t, sol.y[0], label='Exact solution - $u$')
 plt.plot(t, y_pred_list[0], 'o', label='Neural solution - $\hat{u}_1$')
 plt.legend(loc = "best")
@@ -105,65 +68,21 @@ plt.ylabel('Values', fontsize=10)
 plt.xlim((-3, 3.1))
 plt.show()
 
-
+############################# Calculate the mean square error on the training set
 np.mean([sqrt(mean_squared_error(sol.y[i], y_pred_list[i])) for i in range(3)])
 print(np.mean([sqrt(mean_squared_error(sol.y[i], y_pred_list[i])) for i in range(3)]))
 
-
+############################# Output of the constructed solution on the test set
 t_test = np.linspace(-3, 3.5, 250).reshape(-1,1)
-
 sol = solve_ivp(f, [t_test.min(), t_test.max()], y0_list, method='Radau', rtol=1e-5)
 y_pred_list, dydt_pred_list = nn.predict(t=t_test)
 plt.figure(figsize=(8, 6))
-bwith = 0.5
-ax = plt.gca()
-ax.spines['top'].set_color('black')
-ax.spines['right'].set_color('black')
-ax.spines['left'].set_color('black')
-ax.spines['bottom'].set_color('black')
-ax.spines['bottom'].set_linewidth(bwith)
-ax.spines['left'].set_linewidth(bwith)
-ax.spines['top'].set_linewidth(bwith)
-ax.spines['right'].set_linewidth(bwith)
-plt.grid(which='major')
-plt.tick_params(top=False, bottom=True, left=True, right=False)
-plt.tick_params(which='both', direction='in', width=0.5, length=2)
-plt.axis([-3, 3.3, -1, 1])
-plt.xticks(np.arange(-3, 3.5, step=1))
-plt.yticks(np.arange(-2, 1, step=0.3))
-
-
-
 plt.plot(sol.t, sol.y[0], label='Exact solution - $u$')
 plt.plot(t_test, y_pred_list[0], 'o', label='Neural solution - $\hat{u}_1$')
 plt.axvline(x = 3 , linestyle = '--')
 plt.legend(loc = 'best')
 plt.xlabel(r'$\xi$', fontsize=10)
 plt.ylabel('Values', fontsize=10)
-
-
-def plot_loss_compare(model, maxiter=2000, iprint=100):
-    model.reset_weights()
-    save_init_paras_list = model.params_list
-    save_init_flattened_params = model.flattened_params
-
-    methods = ['BFGS']
-    plt.figure(figsize=(8, 6))
-    for method in methods:
-        model.params_list = save_init_paras_list
-        model.flattened_params = save_init_flattened_params
-
-        start_time = time.time()
-        model.train(method=method, maxiter=maxiter, iprint=iprint)
-        duration = time.time() - start_time
-
-        plt.semilogy(range(len(model.loss)), model.loss, label=method + ' (time = {}s'.format(round(duration, 2)) + ')')
-
-    plt.legend()
-    plt.xlabel("Iteration")
-    plt.ylabel("Log Loss")
-    plt.show()
-
-plot_loss_compare(nn)
+plt.show()
 
 
